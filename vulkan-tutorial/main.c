84d2433f36040881abed99a34bc85c6871c71859
DonizeteVida <donizetevida1998@outlook.com>
2024-02-18 22:08:04 -0300

    Initial Commit

---
+++ vulkan-tutorial/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  vulkan-tutorial
+//
+//  Created by Donizete Vida on 18/02/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
